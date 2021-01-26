dc8568307962a8ddfc5394134a2039f0ccfe83fc
jsotoca <jsotoca@unprg.edu.pe>
2021-01-25 23:26:10 -0500

    Modelo usuario creado

---
+++ src/models/user.model.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    fullname: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true}
+},
+{
+    timestamps:true
+});
+
+const User = model('user',userSchema);
+export default User;
